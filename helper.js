536747af016ed801741e05a26befc2a1e5ac6549
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-09-07 13:56:27 +0530

    feat: implement set method with separate chaining collision handling
    - Created linked list chains for collision resolution
    - Insert key-value pairs into linked lists, supporting updates on key match

---
+++ helper.js
@@ -0,0 +1,36 @@
+class Node {
+  key = null;
+  value = null;
+  nextNode = null;
+}
+
+export class LinkedList {
+  head = null;
+  tail = null;
+  totalNodes = 0;
+
+  insert(key, val) {
+    if(this.head === null){
+        this.head = new Node()
+        this.head.key = key
+        this.head.value = val
+        this.tail = this.head
+        this.totalNodes ++
+    } else{
+        let currentNode = this.head
+        for(let i = 0; i < this.totalNodes; i++){
+            if(currentNode.key === key){
+                currentNode.value = val
+                return
+            }
+            currentNode = currentNode.nextNode
+        }
+        currentNode = this.tail
+        currentNode.nextNode = new Node()
+        currentNode.nextNode.key = key;
+        currentNode.nextNode.value = val
+        this.tail = currentNode.nextNode
+        this.totalNodes ++
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+import { LinkedList } from "./helper.js"
+
 class HashMap {
-  loadFactor;
-  capacity;
+
+    constructor(initialCapacity = 16, loadFactor = 0.75){
+        this.buckets = new Array(initialCapacity)
+        this.size = 0
+        this.capacity = initialCapacity
+        this.loadFactor = loadFactor
+    }
 
   hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i) % 16;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
     }
 
     return hashCode;
   }
+  set(key, value){
+    const hashedKey = this.hash(key)
+    if(!this.buckets[hashedKey]){
+        this.buckets[hashedKey] = new LinkedList()
+        this.buckets[hashedKey].insert(key, value) 
+    } else {
+        this.buckets[hashedKey].insert(key,value)
+    }
 }
+}
+let a = new HashMap()
+
+// console.log(a)
